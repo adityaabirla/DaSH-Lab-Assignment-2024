de67c42ba3275d3b833127ca7283d7d87faab169
Aditya Birla <158043799+adityaabirla@users.noreply.github.com>
2024-08-31 12:38:55 +0530

    Update clie1.py

--- Development/clie1.py
+++ Development/clie1.py
@@ -7,7 +7,7 @@ def dump_to_json(data, filename):
     """Dump tuple data to JSON file in append mode"""
     with open(filename, 'a') as f:
         json.dump(data, f,indent=4)
-        f.write('\n')  # append a newline for readability
+        f.write('\n')  
 
 
 
@@ -22,7 +22,7 @@ c.send(bytes(prompti,'utf-8'))
 #decoded_tuple = tuple(s.decode("utf-8") for s in c.recv(10000))
 encoded_tuple = c.recv(10000)
 end_time = int(time.time())
-# Decode the tuple from bytes
+
 my_tuple = pickle.loads(encoded_tuple)
 
 filename = "all_responses_combined.json"
